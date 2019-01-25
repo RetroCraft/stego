8fa2b37bffac265e88df462d51d0845fd6e52aaf
RetroCraft <retrocraft314@gmail.com>
2019-01-24 19:48:32 -0500

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# stego
+
+An automatic steganography tool.
+
+1. Install with `pip install .`.
+2. Run with `stego f filename.png` or `stego u http://youtu.be/qnGuthiTFTU`
+3. Profit.
+
+The Python methods `process_url(url)` and `process_file(filename)` are also exposed in the main module.
--- main.py
+++ main.py
@@ -3,39 +3,26 @@ import click
 import filetype
 import re
 import requests
-import stego.audio
-import stego.image
-import stego.video
-import stego.youtube
+from stego import *
 
 
-@click.command()
-@click.option('-f', '--file', 'filename', type=click.Path(exists=True))
-@click.option('-u', '--url')
-def cli(filename, url):
-    if filename:
-        return process_file(filename)
-    elif url:
-        # youtube-dl
-        try:
-            print('[u] attempting youtube-dl...')
-            f = stego.youtube.download(url)
-            return stego.video.process(f)
-        except:
-            print('[u] youtube-dl failed.')
+@click.group()
+def cli():
+    pass
 
-        # normal download
-        f = ''
-        try:
-            print('[u] attempting raw download...')
-            # determine filename
-            f = process_url(url)
-        except:
-            print('[u] could not download file.')
-        if f:
-            return process_file(f)
 
-    print('[ ] nothing to do. specify -f filename or -u URL')
+@cli.command()
+@click.argument('filename', type=click.Path(exists=True))
+def f(filename):
+    """ process file FILENAME """
+    process_file(filename)
+
+
+@cli.command()
+@click.argument('url')
+def u(url):
+    """ process url URL """
+    process_url(url)
 
 
 def process_file(f):
@@ -43,30 +30,47 @@ def process_file(f):
     if kind:
         print('[f] detected filetype %s' % kind.mime)
         if 'video' in kind.mime:
-            return stego.video.process(f)
+            return video.process(f)
         if 'audio' in kind.mime:
-            return stego.audio.process(f)
+            return audio.process(f)
         if 'image' in kind.mime:
-            return stego.image.process(f)
+            return image.process(f)
     return print('[f] cannot detect file type.')
 
 
 def process_url(u):
-    r = requests.get(u, allow_redirects=True)
-    # find filename
-    filename = 'download'
-    # try url
-    if u.find('/'):
-        filename = u.rsplit('/', 1)[1]
-    # try headers
-    cd = r.headers.get('content-disposition')
-    if cd:
-        fname = re.findall('filename=(.+)', cd)
-        if len(fname) != 0:
-            filename = fname[0]
-    # save file
-    open(filename, 'wb').write(r.content)
-    return filename
+    # youtube-dl
+    try:
+        print('[u] attempting youtube-dl...')
+        f = youtube.download(url)
+        return video.process(f)
+    except:
+        print('[u] youtube-dl failed.')
+
+    # normal download
+    f = ''
+    try:
+        print('[u] attempting raw download...')
+        # determine filename
+        r = requests.get(u, allow_redirects=True)
+        # find filename
+        f = 'download'
+        # try url
+        if u.find('/'):
+            f = u.rsplit('/', 1)[1]
+        # try headers
+        cd = r.headers.get('content-disposition')
+        if cd:
+            fname = re.findall('filename=(.+)', cd)
+            if len(fname) != 0:
+                f = fname[0]
+        # save file
+        open(f, 'wb').write(r.content)
+        print('[u] downloaded to "%s"' % f)
+    except:
+        print('[u] could not download file.')
+    if f:
+        return process_file(f)
 
 
 if __name__ == '__main__':
